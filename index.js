c5a9fd61d0f2e11d29cbe58999bc965e6871686b
A S Harish Kumar <107850160+ASHarish001@users.noreply.github.com>
2025-05-02 22:57:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // Base path to your AnimeEpisodes folder
-const animeDir = "./";
+const animeDir = "./AnimeEpisodes";
 
 const server = http.createServer(async (req, res) => {
   if (req.url === '/api/users') {
